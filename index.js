6b1631c14015325b848b8b6b0cb6c32b6b4f8e2e
jlara <jlaramintic@gmail.com>
2024-07-06 12:40:33 -0500

    commit 6

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import { PORT } from "./config.js";
+import app from "./src/app.js";
+import { PORT } from "./src/config.js";
 
 app.listen(PORT);
 console.log(`Server on port http://localhost:${PORT}`);
